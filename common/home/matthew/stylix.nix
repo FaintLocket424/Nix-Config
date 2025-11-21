33ad0be7d6ffa4cf07410b0e2a7bf772089cf2f7
FaintLocket424 <github.grill135@passinbox.com>
2025-11-21 14:44:24 +0000

    Automatic commit from update script

--- common/home/matthew/stylix.nix
+++ common/home/matthew/stylix.nix
@@ -5,32 +5,7 @@
   stylix = {
     enable = true;
     image = ./backgrounds/toothless_1.jpg;
-
     base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-material-dark-hard.yaml";
-
-    fonts = {
-      sizes = {
-        desktop = 11;
-        popups = 11;
-        terminal = 10;
-        applications = 11;
-      };
-
-      sansSerif = {
-        package = pkgs.inter;
-        name = "Inter";
-      };
-
-      serif = {
-        package = pkgs.roboto-serif;
-        name = "Roboto Serif";
-      };
-
-      monospace = {
-        package = pkgs.nerd-fonts.commit-mono;
-        name = "CommitMono Nerd Font";
-      };
-    };
 
     polarity = "dark";
     opacity.popups = 0.4;

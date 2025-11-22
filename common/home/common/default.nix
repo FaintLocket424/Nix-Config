30e8fbc3f512147b8ec4b476663945d0c3ad8276
Matthew Peters <github.grill135@passinbox.com>
2025-11-22 16:06:58 +0000

    Automatic commit from update script

--- common/home/common/default.nix
+++ common/home/common/default.nix
@@ -39,7 +39,7 @@ in {
       vesktop # Lightweight linux discord client
       geteduroam # Software for getting on eduroam
       parsec-bin # Remote Desktop
-      bc # Calculator
+      gnome-calculator # Calculator
 
       # QT Fixes/packages
       kdePackages.qtwayland

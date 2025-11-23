2bd9bddff0d1b1639f59ccf7aa3207f2de033023
FaintLocket424 <github.grill135@passinbox.com>
2025-11-23 11:08:48 +0000

    Automatic commit from update script

--- common/home/common/scripts/rebuild.sh
+++ common/home/common/scripts/rebuild.sh
@@ -3,7 +3,7 @@
 cd /home/matthew/programming/Production/nixos-config || exit
 git add ./*;
 git commit -a -m "Automatic commit from update script";
-git pull
+git pull;
 git push;
 nix flake update --flake .;
 sudo nixos-rebuild switch --flake .#"$(hostname)";
--- common/home/common/scripts/rebuild.sync-conflict-20251123-083353-T66QOX3.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env fish
-
-cd /home/matthew/programming/Production/nixos-config || exit
-git add ./*;
-git commit -a -m "Automatic commit from update script";
-git pull
-git push;
-nix flake update --flake .;
-sudo nixos-rebuild switch --flake .#"$(hostname)";

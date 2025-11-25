a81c7000fbc9366839f9fafe794bb18003d73112
Matthew Peters <github.grill135@passinbox.com>
2025-11-25 10:55:47 +0000

    Automatic commit from update script

--- falcon/configuration.nix
+++ falcon/configuration.nix
@@ -16,58 +16,58 @@
     useOSProber = true;
   };
 
-  boot.kernelModules = [
-    "nvidia"
-  ];
+#  boot.kernelModules = [
+#    "nvidia"
+#  ];
 
-  hardware.graphics.extraPackages = with pkgs; [
-    nvidia-vaapi-driver
-  ];
+#  hardware.graphics.extraPackages = with pkgs; [
+#    nvidia-vaapi-driver
+#  ];
 
-  services.xserver.videoDrivers = [ "nvidia" ];
+#  services.xserver.videoDrivers = [ "nvidia" ];
 
-  hardware.nvidia = {
-    modesetting.enable = true;
-    powerManagement.enable = true;
-    powerManagement.finegrained = false;
-    open = true;
-    nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.production;
-  };
+#  hardware.nvidia = {
+#    modesetting.enable = true;
+#    powerManagement.enable = true;
+#    powerManagement.finegrained = false;
+#    open = true;
+#    nvidiaSettings = true;
+#    package = config.boot.kernelPackages.nvidiaPackages.production;
+#  };
 
-  systemd.services = {
-    "gnome-suspend" = {
-      description = "suspend gnome shell";
-      before = [
-        "systemd-suspend.service"
-        "systemd-hibernate.service"
-        "nvidia-suspend.service"
-        "nvidia-hibernate.service"
-      ];
-      wantedBy = [
-        "systemd-suspend.service"
-        "systemd-hibernate.service"
-      ];
-      serviceConfig = {
-        Type = "oneshot";
-        ExecStart = ''${pkgs.procps}/bin/pkill -f -STOP ${pkgs.gnome-shell}/bin/gnome-shell'';
-      };
-    };
-    "gnome-resume" = {
-      description = "resume gnome shell";
-      after = [
-        "systemd-suspend.service"
-        "systemd-hibernate.service"
-        "nvidia-resume.service"
-      ];
-      wantedBy = [
-        "systemd-suspend.service"
-        "systemd-hibernate.service"
-      ];
-      serviceConfig = {
-        Type = "oneshot";
-        ExecStart = ''${pkgs.procps}/bin/pkill -f -CONT ${pkgs.gnome-shell}/bin/gnome-shell'';
-      };
-    };
-  };
+#  systemd.services = {
+#    "gnome-suspend" = {
+#      description = "suspend gnome shell";
+#      before = [
+#        "systemd-suspend.service"
+#        "systemd-hibernate.service"
+#        "nvidia-suspend.service"
+#        "nvidia-hibernate.service"
+#      ];
+#      wantedBy = [
+#        "systemd-suspend.service"
+#        "systemd-hibernate.service"
+#      ];
+#      serviceConfig = {
+#        Type = "oneshot";
+#        ExecStart = ''${pkgs.procps}/bin/pkill -f -STOP ${pkgs.gnome-shell}/bin/gnome-shell'';
+#      };
+#    };
+#    "gnome-resume" = {
+#      description = "resume gnome shell";
+#      after = [
+#        "systemd-suspend.service"
+#        "systemd-hibernate.service"
+#        "nvidia-resume.service"
+#      ];
+#      wantedBy = [
+#        "systemd-suspend.service"
+#        "systemd-hibernate.service"
+#      ];
+#      serviceConfig = {
+#        Type = "oneshot";
+#        ExecStart = ''${pkgs.procps}/bin/pkill -f -CONT ${pkgs.gnome-shell}/bin/gnome-shell'';
+#      };
+#    };
+#  };
 }

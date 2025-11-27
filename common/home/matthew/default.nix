c3c0143350442a277976011c86a9e5133b87d48e
Matthew Peters <github.grill135@passinbox.com>
2025-11-27 12:34:47 +0000

    Automatic commit from update script

--- common/home/matthew/default.nix
+++ common/home/matthew/default.nix
@@ -141,6 +141,25 @@
         home = {
           id = 0;
 
+          search = {
+            default = "ddg";
+            engines = {
+              nix-packages = {
+                name = "Nix Packages";
+                urls = [{
+                  template = "https://search.nixos.org/packages";
+                  params = [
+                    { name = "type"; value = "packages"; }
+                    { name = "query"; value = "{searchTerms}"; }
+                  ];
+                }];
+
+                icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+                definedAliases = [ "@np" ];
+              };
+            };
+          };
+
           bookmarks = {
             force = true;
             settings = [

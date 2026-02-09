04d42e897ee68844c329e0974acac0becf220a5a
FaintLocket424 <github.grill135@passinbox.com>
2026-02-09 15:16:05 +0000

    Automatic commit from update script

--- users/core/scripts/rebuild-system.fish
+++ users/core/scripts/rebuild-system.fish
@@ -5,40 +5,44 @@ set CONFIG_DIR "$HOME/programming/Production/nixos-config"
 cd $CONFIG_DIR || exit 1
 
 # Handle arguments
-set MODE switch
+set MODE "switch"
 set EXTRA_FLAGS ""
+set HOST (hostname)
+set OFFLINE 0
 
 for arg in $argv
     switch $arg
         case -c --check
             echo "Running in CHECK mode (dry-build)..."
-            # dry-build: shows what would happen without applying
-            # --offline: prevents downloading new binaries
-            set MODE dry-build
-            set EXTRA_FLAGS --offline
+            set MODE "dry-build"
+            set EXTRA_FLAGS "--offline"
         case -o --offline
             echo "Running in OFFLINE mode"
-            set EXTRA_FLAGS --offline
+            set EXTRA_FLAGS "--offline"
+            set OFFLINE 1
         case "*"
             echo "Unknown argument: $arg"
             exit 1
     end
 end
 
+git add -A
+
 echo "Formatting..."
 nix fmt
-
-# Essential: git add -A so Nix sees new/untracked files
-git add -A
 
 if not git diff --cached --quiet
     echo "Changes detected, committing..."
     git commit -m "Automatic commit from update script"
 
-    echo "Syncing with remote..."
-    # pull/push logic with "or true" to ensure the script doesn't crash on network issues
-    git pull --rebase; or echo "Warning: git pull failed"
-    git push; or echo "Warning: git push failed"
+    if test $OFFLINE -eq 0
+      echo "Offline mode, skipping git pull & push"
+    else
+      echo "Syncing with remote..."
+      git pull --rebase; or echo "Warning: git pull failed"
+      git push; or echo "Warning: git push failed"
+    end
+
 else
     echo "No changes to commit."
 end

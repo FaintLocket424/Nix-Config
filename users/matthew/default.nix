15d8a73bb232f4655fdbb22ba94695cad1e7cd69
FaintLocket424 <github.grill135@passinbox.com>
2026-03-09 16:36:42 +0000

    Automatic commit from update script

--- users/matthew/default.nix
+++ users/matthew/default.nix
@@ -35,6 +35,7 @@
       obsidian # FOSS for Markdown Note-taking
       scrcpy # Android screen mirroring
       #      zotero # Research reference manager
+      mcaselector # Tool for filtering chunks in Minecraft worlds
     ];
   };
 

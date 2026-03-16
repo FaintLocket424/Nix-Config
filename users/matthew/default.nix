7ba55a80e1e959427d84e3f65db270f6b5f5717e
FaintLocket424 <github.grill135@passinbox.com>
2026-03-16 09:53:26 +0000

    Automatic commit from update script

--- users/matthew/default.nix
+++ users/matthew/default.nix
@@ -34,6 +34,7 @@
       obs-studio # FOSS for Screen Recording and Streaming
       obsidian # FOSS for Markdown Note-taking
       scrcpy # Android screen mirroring
+      taskwarrior # Terminal Project Manager
       #      zotero # Research reference manager
       #mcaselector # Tool for filtering chunks in Minecraft worlds
     ];

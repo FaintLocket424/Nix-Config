3e889e8aff5e4d7cc29043709a833d45488f46a9
FaintLocket424 <github.grill135@passinbox.com>
2026-04-15 23:37:58 +0100

    added Hyperion device id to syncthing

--- users/matthew/syncthing.nix
+++ users/matthew/syncthing.nix
@@ -9,7 +9,7 @@
 
     settings.devices = {
       "Falcon" = { id = "NQWWU2G-FMO27SD-BD4P3KO-RNJTTUS-52XVHGA-AKSFE76-I5HU6SM-2RDTMAH"; };
-      #   "Hyperion" = { id = ""; };
+      "Hyperion" = { id = "T66QOX3-NADLDBH-UBAUUVR-U53YNCA-2GVR4JI-MS3RF4U-WWHHBGZ-YJYWWAU"; };
     };
   };
 }

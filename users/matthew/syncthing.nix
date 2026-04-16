4003fd6d327baffc220ce42f0a0458678802d5e9
FaintLocket424 <github.grill135@passinbox.com>
2026-04-16 01:04:43 +0100

    added dolphin emulator to syncthing

--- users/matthew/syncthing.nix
+++ users/matthew/syncthing.nix
@@ -1,15 +1,32 @@
 { pkgs, config, ... }: {
   services.syncthing = {
     enable = true;
-
-    # overrideFolders = true;
+    overrideFolders = true;
     overrideDevices = true;
 
-    # settings.folders = { };
+    settings = {
+      openDefaultPorts = true;
+      options.localAnnounceEnabled = true;
+      options.urAccepted = -1;
 
-    settings.devices = {
-      "Falcon" = { id = "NQWWU2G-FMO27SD-BD4P3KO-RNJTTUS-52XVHGA-AKSFE76-I5HU6SM-2RDTMAH"; };
-      "Hyperion" = { id = "T66QOX3-NADLDBH-UBAUUVR-U53YNCA-2GVR4JI-MS3RF4U-WWHHBGZ-YJYWWAU"; };
+      folders = {
+        "Dolphin Emulator Wii Saves" = {
+          path = "${config.home.homeDirectory}/.local/share/dolphin-emu/Wii/title";
+          id = "dolphin-emu-wii-saves";
+          devices = [ "Falcon" "Hyperion" ];
+        };
+
+        "Dolphin Emulator Texture Packs" = {
+          path = "${config.home.homeDirectory}/.local/share/dolphin-emu/Load/Textures";
+          id = "dolphin-emu-load-textures";
+          devices = [ "Falcon" "Hyperion" ];
+        };
+      };
+
+      devices = {
+        "Falcon" = { id = "NQWWU2G-FMO27SD-BD4P3KO-RNJTTUS-52XVHGA-AKSFE76-I5HU6SM-2RDTMAH"; };
+        "Hyperion" = { id = "T66QOX3-NADLDBH-UBAUUVR-U53YNCA-2GVR4JI-MS3RF4U-WWHHBGZ-YJYWWAU"; };
+      };
     };
   };
 }

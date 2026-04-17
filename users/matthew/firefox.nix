d29b9a51975cd309ce6710543894558c355cb855
FaintLocket424 <github.grill135@passinbox.com>
2026-04-18 00:50:03 +0100

    updated firefox bookmarks

--- users/matthew/firefox.nix
+++ users/matthew/firefox.nix
@@ -11,12 +11,12 @@
           bookmarks = [
             { name = "YouTube"; url = "https://www.youtube.com/feed/subscriptions"; }
             { name = "YouTube Music"; url = "https://music.youtube.com/"; }
+            { name = "Floatplane"; url = "https://www.floatplane.com/channel/linustechtips/home"; }
             {
               name = "Video";
               bookmarks = [
                 { name = "Netflix"; url = "https://www.netflix.com/browse"; }
                 { name = "Disney+"; url = "https://www.disneyplus.com/en-gb/home"; }
-                { name = "BBC iPlayer"; url = "https://www.bbc.co.uk/iplayer"; }
               ];
             }
             {
@@ -33,27 +33,28 @@
                 { name = "Drive"; url = "https://drive.proton.me"; }
               ];
             }
-            {
-              name = "NixOS";
-              bookmarks = [
-                { name = "NixOS Packages"; url = "https://search.nixos.org/packages?channel=25.11&"; }
-                { name = "Home Manager Options"; url = "https://home-manager-options.extranix.com/?query=&release=release-25.11"; }
-                { name = "NixOS Options"; url = "https://search.nixos.org/options?channel=25.11&"; }
-              ];
-            }
+            # {
+            #   name = "NixOS";
+            #   bookmarks = [
+            #     { name = "NixOS Packages"; url = "https://search.nixos.org/packages?channel=25.11&"; }
+            #     { name = "Home Manager Options"; url = "https://home-manager-options.extranix.com/?query=&release=release-25.11"; }
+            #     { name = "NixOS Options"; url = "https://search.nixos.org/options?channel=25.11&"; }
+            #   ];
+            # }
+            { name = "LTT Labs"; url = "https://www.lttlabs.com/"; }
             { name = "DHM Weather"; url = "https://www.metoffice.gov.uk/weather/forecast/gcwzefp2c"; }
             { name = "WhatsApp"; url = "https://web.whatsapp.com/"; }
             { name = "Immich"; url = "http://100.70.23.49:2283/"; }
             { name = "Jellyfin"; url = "http://100.70.23.49:8096/"; }
-            { name = "Developer Roadmaps"; url = "https://roadmap.sh/"; }
-            {
-              name = "Pebble";
-              bookmarks = [
-                { name = "Learning C with Pebble"; url = "https://pebble.gitbooks.io/learning-c-with-pebble/content/chapter04.html"; }
-                { name = "Tutorials // Pebble Developers"; url = "https://developer.repebble.com/tutorials/"; }
-                { name = "CloudPebble"; url = "https://cloudpebble.repebble.com/ide/"; }
-              ];
-            }
+            # { name = "Developer Roadmaps"; url = "https://roadmap.sh/"; }
+            # {
+            #   name = "Pebble";
+            #   bookmarks = [
+            #     { name = "Learning C with Pebble"; url = "https://pebble.gitbooks.io/learning-c-with-pebble/content/chapter04.html"; }
+            #     { name = "Tutorials // Pebble Developers"; url = "https://developer.repebble.com/tutorials/"; }
+            #     { name = "CloudPebble"; url = "https://cloudpebble.repebble.com/ide/"; }
+            #   ];
+            # }
             {
               name = "MCM";
               bookmarks = [
@@ -61,7 +62,7 @@
                 { name = "Pebblehost"; url = "https://panel.pebblehost.com/server/3e437777"; }
               ];
             }
-            { name = "Stop Misusing Rate Limiting in Go - 5 Real-World Fixes"; url = "https://www.linkedin.com/pulse/stop-writing-dumb-rate-limiters-go-do-thisinstead-archit-agarwal-xjuac"; }
+            # { name = "Stop Misusing Rate Limiting in Go - 5 Real-World Fixes"; url = "https://www.linkedin.com/pulse/stop-writing-dumb-rate-limiters-go-do-thisinstead-archit-agarwal-xjuac"; }
           ];
         }
       ];

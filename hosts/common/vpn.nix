2722d4b682a97fc882a34625ff220b8eb16c5e45
FaintLocket424 <github.grill135@passinbox.com>
2026-04-30 23:09:57 +0100

    possibly fixed proton vpn wireguard config

--- hosts/common/vpn.nix
+++ hosts/common/vpn.nix
@@ -7,28 +7,43 @@
     firewall.checkReversePath = "loose";
 
     wireguard.interfaces."wg-proton" = {
-      ips = [ "10.x.x.x/32" ];
+      ips = [
+        "10.2.0.2/32"
+        "2a07:b944::2:2/128"
+      ];
+
       privateKeyFile = "/run/secrets/proton.key";
 
       routes = [
         { address = "0.0.0.0"; prefixLength = 1; }
         { address = "128.0.0.0"; prefixLength = 1; }
+        { address = "::"; prefixLength = 1; }
+        { address = "8000::"; prefixLength = 1; }
       ];
 
       peers = [
         {
           publicKey = "AFp36cKCIznWgRchU9fE2G9kPK6zcdS+7S/u4drPU1g=";
-          endpoint = "HOST:PORT";
+          endpoint = "146.70.48.2:51820";
 
           allowedIPs = [
             "0.0.0.0/1"
             "128.0.0.0/1"
+
+            "::/1"
+            "8000::/1"
           ];
+
+          persistentKeepalive = 25;
         }
       ];
     };
   };
 
   services.tailscale.enable = true;
-  services.resolved.enable = true;
+
+  services.resolved = {
+    enable = true;
+    dnssec = "false";
+  };
 }

2ad978f63a9ae3be8dc678b2251fdff2ee8c2c76
FaintLocket424 <github.grill135@passinbox.com>
2026-05-01 00:18:36 +0100

    vpn

--- hosts/common/vpn.nix
+++ hosts/common/vpn.nix
@@ -1,45 +1,7 @@
 { pkgs, ... }: {
-  # environment.systemPackages = with pkgs; [
-  # protonvpn-gui
-  # ];
+  environment.systemPackages = with pkgs; [
+    protonvpn-gui
+  ];
 
-  networking = {
-    firewall.checkReversePath = "loose";
-
-    wireguard.interfaces = {
-      wg0 = {
-        ips = [
-          "10.2.0.2/32"
-          "2a07:b944::2:2/128"
-        ];
-
-        privateKeyFile = "/run/secrets/wg-proton-private.key";
-
-        listenPort = 51820;
-
-        peers = [
-          {
-            publicKey = "AFp36cKCIznWgRchU9fE2G9kPK6zcdS+7S/u4drPU1g=";
-
-            allowedIPs = [
-              "0.0.0.0/0"
-              "::/0"
-            ];
-
-            endpoint = "146.70.48.2:51820";
-
-            persistentKeepalive = 25;
-          }
-        ];
-      };
-    };
-
-    nameservers = [ "10.2.0.1" "2a07:b944::2:1" ];
-  };
-
-  # services.tailscale.enable = true;
-
-  services.resolved = {
-    enable = true;
-  };
+  services.resolved.enable = true;
 }
